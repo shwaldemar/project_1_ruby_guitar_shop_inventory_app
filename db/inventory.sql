cfdbaa2d8653d54932ec0ed03671c5c83179b7d9
Simon <shwaldemar@gmail.com>
2019-06-12 15:06:12 +0100

    id's in edit for products and suppliers put in to p tags rather than edit options so users cant change quantity and end up with the wrong supplier by accident

--- db/inventory.sql
+++ db/inventory.sql
@@ -3,16 +3,16 @@ DROP TABLE suppliers;
 
 CREATE TABLE suppliers (
   id SERIAL8 primary key,
-  name VARCHAR(255) not null
+  name VARCHAR(255)
 );
 
 CREATE TABLE  products (
   id SERIAL8 primary key,
-  name VARCHAR(255) not null,
+  name VARCHAR(255) ,
   model VARCHAR(255),
   description VARCHAR(255),
   buy_price INT8,
   sell_price INT8,
   quantity INT8,
-  supplier_id INT8 references suppliers(id)
+  supplier_id INT8 references suppliers(id) ON DELETE CASCADE
 );
